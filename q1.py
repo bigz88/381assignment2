b1fb0eebe6012703652600b8cf407909a501f10d
FayzanT <fayzan.toor@ucalgary.ca>
2023-02-09 16:44:29 -0700

    Fibbin our naccis

--- q1.py
+++ q1.py
@@ -1,7 +1,14 @@
 # Exercise 1
 # Zaid Ahmed and Fayzan Toor
-def func(n):
+# Write fib(n)
+def func(n, cache={}):
     if n == 0 or n == 1:
         return n
     else:
-        return func(n-1) + func(n-2)
+        if n in cache:
+            return cache[n]
+        else:
+            cache[n] = func(n-1) + func(n-2)
+            return cache[n]
+
+
